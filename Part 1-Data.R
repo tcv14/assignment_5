516c175f1285b526b73df4cf0ad3b0e181fd9544
Tiffany Cheng <tycheng@bu.edu>
2018-03-05 15:19:27 -0500

    added comments

--- Part 1-Data.R
+++ Part 1-Data.R
@@ -1,14 +1,17 @@
 library(tidyverse)
 library(stringr)
 
+# preparing to read in data from website
 url1 <- "http://www.ndbc.noaa.gov/view_text_file.php?filename=46035h"
 url2 <- ".txt.gz&dir=data/historical/stdmet/"
 
+# first group of data (1985 to 2006)
 years.pt1 <- c(1985:2006)
 urls.pt1 <- str_c(url1, years.pt1, url2, sep = "")
 filenames <- str_c(years.pt1, sep = "")
 N <- length(urls.pt1)
 
+# use for loop to read in data
 for (i in 1:N){
   suppressMessages(
     assign(filenames[i], read_table(urls.pt1[i], col_names = TRUE))
@@ -17,7 +20,7 @@ for (i in 1:N){
   file <- get(filenames[i])
   
   colnames(file)[1] <- "YYYY"
-  file[1] <- sapply(file[1],function(x) {
+  file[1] <- sapply(file[1],function(x) { # add 19 to beginning of years where there is no 19
     if (file[1,1]<100) {
       mutate(file[1]+1900)
     }
@@ -26,20 +29,24 @@ for (i in 1:N){
     }
     })
   
-  file$TIDE <- NA
+  file$TIDE <- NA # add the column for TIDE and there are no values yet
   
-  file$mm <- "00"
+  file$mm <- "00" # add the column for minutes (mm) and there are no values yet
   
+  # WSDP and GST are read in as one column, so have to split them up
   file$`WSPD` <- str_split_fixed(file$`WSPD GST`," ",2)[,1]
   file$`GST` <- str_split_fixed(file$`WSPD GST`," ",2)[,2]
   file <- file %>% select(-`WSPD GST`)
-  
+  # rename newly created columns
   file <- file %>% dplyr::rename("WDIR"="WD","PRES"="BAR")
   
+  # reorder columns to match website
   file <- file[c(1:4,16,5,17:18,6:15)]
   
+  # filter down to one observation at noon time
   file <- file %>% filter(file$hh==12)
-
+  
+  # save data altogether in object called bouy.pt1 as data is read in
   if (i == 1) {
     bouy.pt1 <- file
   }
@@ -48,12 +55,13 @@ for (i in 1:N){
   }
 }
 
-
+# second group of data (2007 to 2017, no data available for 2013)
 years.pt2 <- c(2007:2012,2014:2017)
 urls.pt2 <- str_c(url1, years.pt2, url2, sep = "")
 filenames <- str_c(years.pt2, sep = "")
 N <- length(urls.pt2)
 
+# use for loop to read in data
 for (i in 1:N){
   suppressMessages(
     assign(filenames[i], read_table(urls.pt2[i], col_names = TRUE))
@@ -63,25 +71,27 @@ for (i in 1:N){
   
   colnames(file)[1] <- "YYYY"
   
+  # WDIR, WSDP, and GST are read in as one column, so have to split them up
   values <- unlist(regmatches(file$`WDIR WSPD GST`,gregexpr("[[:digit:]]+\\.*[[:digit:]]*",file$`WDIR WSPD GST`)))
   file$`WDIR` <- values[seq(1,length(values),by=3)]
   file$`WSPD` <- values[seq(2,length(values),by=3)]
   file$`GST` <- values[seq(3,length(values),by=3)]
   file <- file %>% select(-`WDIR WSPD GST`)
   
+  # MWD and PRES also read in as one column, so have to split them
   if (colnames(file[9])=="MWD   PRES") {
     file$MWD <- str_split_fixed(file$`MWD   PRES`," ",2)[,1]
     file$PRES <- str_split_fixed(file$`MWD   PRES`," ",2)[,2]
     file <- file %>% select(-`MWD   PRES`)
   }
   
-  file12 <- file %>% filter(file$hh=="12" & file$mm=="00")
-  file1150 <- file %>% filter(file$hh=="11" & file$mm=='50')
+  # filter down to one observation at noon time or as close to noon time as possible
+  file <- file %>% filter((file$hh=="12" & file$mm=="00")|(file$hh=="11" & file$mm=='50'))
   
-  file <- rbind(file12,file1150)
-  
+  # reorder columns to match website
   file <- file[c(1:5,14:16,6:8,17:18,9:13)]
   
+  # save data altogether in object called bouy.pt1 as data is read in
   if (i == 1) {
     bouy.pt2 <- file
   }
@@ -90,8 +100,11 @@ for (i in 1:N){
   }
 }
 
+# row bind the two groups of data together
 bouy <- rbind(bouy.pt1,bouy.pt2)
 
+# tidying data (using NAs where there are unreasonable data values,
+# selecting columns of interest, and uniting columns to form a date)
 bouy.tidy <- bouy %>% 
   mutate(MWD=replace(MWD,MWD=="999",NA)) %>%
   mutate(ATMP=replace(ATMP,ATMP=="999.0",NA)) %>%
@@ -118,5 +131,6 @@ bouy.tidy <- bouy %>%
   select(1:3,14:15) %>%
   unite(Date,1:3,sep="-")
 
+# convert columns into numeric values
 bouy.tidy$ATMP <- as.numeric(bouy.tidy$ATMP)
 bouy.tidy$WTMP <- as.numeric(bouy.tidy$WTMP)
