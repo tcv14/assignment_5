5da7ba44ae557de8d66cee5f9045bc6dc83b00fd
xrlyu <xlyu@bu.edu>
2018-03-12 13:05:39 -0400

    Changes to script.

--- Part 1-Shiny.R
+++ Part 1-Shiny.R
@@ -1,29 +1,28 @@
 # load required packages
-library(plyr)
-library(tidyverse)
 library(shiny)
 library(shinydashboard)
-source('Part 1.R')
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Bouy 46035"),
+  dashboardHeader(title = "NOAA Bouy 46035"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Air Temperature", tabName = "ATMP", icon = icon("dashboard")),
-      menuItem("Sea Temperature", tabName = "WTMP", icon = icon("th"))
+      menuItem("Air Temperature", tabName = "ATMP", icon = icon("cloud",lib = "glyphicon")),
+      menuItem("Sea Temperature", tabName = "WTMP", icon = icon("tint", lib = "glyphicon"))
     )
   ),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
+    # Boxes need to be put in a row
     tabItems(
       # First tab content
+      # Create a box for the plot
       tabItem(tabName = "ATMP",
               h2("Time Series Plot of Air Temperature"),
               fluidRow(
                 box(plotOutput("plot1", height = 250)),
                 
+                # Create a box for the slider
                 box(
-                  title = "Select Time Frame",
+                  title = "Select Time Frame:",
                   sliderInput("slider1", "Year:", min = 1985, max = 2017,
                               value = c(1995,2000))
                 )
@@ -31,13 +30,15 @@ ui <- dashboardPage(
       ),
       
       # Second tab content
+      # Create a box for the plot
       tabItem(tabName = "WTMP",
               h2("Time Series Plot of Sea Temperature"),
               fluidRow(
                 box(plotOutput("plot2", height = 250)),
                 
+                # Create a box for the slider
                 box(
-                  title = "Select Time Frame",
+                  title = "Select Time Frame:",
                   sliderInput("slider2", "Year:", min = 1985, max = 2017,
                               value = c(1995,2000))
                 )
@@ -48,67 +49,35 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+  source("Part 1.R",local=TRUE)
+  
   # Plot time series of ATMP vs. Date
   output$plot1 <- renderPlot({
-<<<<<<< HEAD
-    tidy.ATMP <- dplyr::filter(tidy.shiny,Year %in% min(input$slider1):max(input$slider1))
-    ggplot(tidy.ATMP,aes(Date, ATMP)) + geom_line(na.rm=TRUE) +
-=======
-  source("Part 1.R",local=TRUE)
-  
-  output$plot1 <- renderPlot({
     selected1 <- tidy.shiny$Year==min(input$slider1):max(input$slider1)
-=======
-    selected1 <- suppressWarnings(tidy.shiny$Year==min(input$slider1):max(input$slider1))
->>>>>>> parent of 00c16e2... removed suppress messages
-=======
-  source("Part 1.R",local=TRUE)
-  
-  output$plot1 <- renderPlot({
-    selected1 <- tidy.shiny$Year==min(input$slider1):max(input$slider1)
->>>>>>> parent of eeeac39... added comments and suppressed warnings
     year.selected1 <- tidy.shiny$Year[which(selected1)]
     ATMP.selected <- tidy.shiny$ATMP[which(selected1)]
+    
     tidy.ATMP <- dplyr::bind_cols(data.frame(year.selected1),data.frame(ATMP.selected),
                                   data.frame(tidy.shiny$Date[which(selected1)])) %>%
       dplyr::rename(Date=`tidy.shiny.Date.which.selected1..`)
+
     ggplot(tidy.ATMP,aes(Date, ATMP.selected)) + geom_line() +
-<<<<<<< HEAD
->>>>>>> parent of eeeac39... added comments and suppressed warnings
-=======
->>>>>>> parent of eeeac39... added comments and suppressed warnings
       ylab('Air Temperature') + scale_x_date(date_breaks = '1 year',date_labels = '%b %y') +
-      theme(axis.text.x=element_text(angle=90, hjust=1))
+      theme(axis.text.x=element_text(angle=65, hjust=1))
   })
   
   output$plot2 <- renderPlot({
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    tidy.WTMP <- dplyr::filter(tidy.shiny,Year %in% min(input$slider2):max(input$slider2))
-    ggplot(tidy.WTMP,aes(Date, WTMP)) + geom_line(na.rm=TRUE) +
-=======
     selected2 <- tidy.shiny$Year==min(input$slider2):max(input$slider2)
-=======
-    selected2 <- suppressWarnings(tidy.shiny$Year==min(input$slider2):max(input$slider2))
->>>>>>> parent of 00c16e2... removed suppress messages
-=======
-    selected2 <- tidy.shiny$Year==min(input$slider2):max(input$slider2)
->>>>>>> parent of eeeac39... added comments and suppressed warnings
     year.selected2 <- tidy.shiny$Year[which(selected2)]
     WTMP.selected <- tidy.shiny$WTMP[which(selected2)]
+    
     tidy.WTMP <- dplyr::bind_cols(data.frame(year.selected2),data.frame(WTMP.selected),
                                   data.frame(tidy.shiny$Date[which(selected2)])) %>%
       dplyr::rename(Date=`tidy.shiny.Date.which.selected2..`)
+    
     ggplot(tidy.WTMP,aes(Date, WTMP.selected)) + geom_line() +
-<<<<<<< HEAD
->>>>>>> parent of eeeac39... added comments and suppressed warnings
-=======
->>>>>>> parent of eeeac39... added comments and suppressed warnings
       ylab('Sea Temperature') + scale_x_date(date_breaks = '1 year',date_labels = '%b %y') +
-      theme(axis.text.x=element_text(angle=90, hjust=1))
+      theme(axis.text.x=element_text(angle=65, hjust=1))
   })
 }
 
--- Part 1.R
+++ Part 1.R
@@ -131,7 +131,6 @@ rm(ldf)
 # save workspace as RData file
 # save.image(file="./Data/part1_tidydata.RData")
 
-------------------------------------------------------------------------
 # directly load this file
 load("./Data/part1_tidydata.RData") 
 
@@ -176,5 +175,5 @@ oneway.test(WTMP ~ Year, data = tidy) # significant change in sea temperature
 tidy.com$Year <- as.factor(format(as.Date(tidy.com$Date), "%Y"))
 
 oneway.test(ATMP ~ Year, data = tidy.com) # significant change in air temperature
-oneway.test(WTMP ~ Year, data = tidy.com) # significant change in air temperature
+oneway.test(WTMP ~ Year, data = tidy.com) # significant change in sea temperature
 
