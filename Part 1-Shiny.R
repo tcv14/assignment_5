db9aefea4ec871f32baad56b16e3f81ccdc6ae6c
xrlyu <xlyu@bu.edu>
2018-03-12 12:57:18 -0400

    Revert "removed suppress messages"
    This reverts commit 00c16e201ef2fc21a1e2a2cde2e49d1f8a1e1dfc.

--- Part 1-Shiny.R
+++ Part 1-Shiny.R
@@ -51,6 +51,7 @@ server <- function(input, output) {
 <<<<<<< HEAD
   # Plot time series of ATMP vs. Date
   output$plot1 <- renderPlot({
+<<<<<<< HEAD
     tidy.ATMP <- dplyr::filter(tidy.shiny,Year %in% min(input$slider1):max(input$slider1))
     ggplot(tidy.ATMP,aes(Date, ATMP)) + geom_line(na.rm=TRUE) +
 =======
@@ -58,6 +59,9 @@ server <- function(input, output) {
   
   output$plot1 <- renderPlot({
     selected1 <- tidy.shiny$Year==min(input$slider1):max(input$slider1)
+=======
+    selected1 <- suppressWarnings(tidy.shiny$Year==min(input$slider1):max(input$slider1))
+>>>>>>> parent of 00c16e2... removed suppress messages
     year.selected1 <- tidy.shiny$Year[which(selected1)]
     ATMP.selected <- tidy.shiny$ATMP[which(selected1)]
     tidy.ATMP <- dplyr::bind_cols(data.frame(year.selected1),data.frame(ATMP.selected),
@@ -71,10 +75,14 @@ server <- function(input, output) {
   
   output$plot2 <- renderPlot({
 <<<<<<< HEAD
+<<<<<<< HEAD
     tidy.WTMP <- dplyr::filter(tidy.shiny,Year %in% min(input$slider2):max(input$slider2))
     ggplot(tidy.WTMP,aes(Date, WTMP)) + geom_line(na.rm=TRUE) +
 =======
     selected2 <- tidy.shiny$Year==min(input$slider2):max(input$slider2)
+=======
+    selected2 <- suppressWarnings(tidy.shiny$Year==min(input$slider2):max(input$slider2))
+>>>>>>> parent of 00c16e2... removed suppress messages
     year.selected2 <- tidy.shiny$Year[which(selected2)]
     WTMP.selected <- tidy.shiny$WTMP[which(selected2)]
     tidy.WTMP <- dplyr::bind_cols(data.frame(year.selected2),data.frame(WTMP.selected),
