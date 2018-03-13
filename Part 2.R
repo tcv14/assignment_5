ea41019f063414672dd583e1e7196c79c54c1aee
Tiffany Cheng <tycheng@bu.edu>
2018-03-13 14:10:31 -0400

    added toxicity levels in and joined data

--- Part 2.R
+++ Part 2.R
@@ -10,7 +10,7 @@ a <- apply(veg, 2, n_distinct)
 b <- names(a[a>1])
 veg.tidy <- veg %>% 
   select(b) %>%
-  dplyr::rename(Geography = `Geo Level`, State = `State ANSI`,
+  dplyr::rename(Area = `Geo Level`, State = `State ANSI`,
                 Data = `Data Item`, Category = `Domain Category`) %>%
   separate(Category, into = c("Label", "Type"), sep=",") %>%
   separate(Data, into=c("A","Class Desc"),sep=" - ") %>%
@@ -19,8 +19,8 @@ veg.tidy <- veg %>%
   separate(Domain,into=c("Domain","B"),sep=", ") %>%
   dplyr::rename(Type=`B`,Chemical=`Type`) %>%
   separate(Chemical, into=c("C","Active Ingrediant or Action Taken"),sep=": ") %>%
-  separate("Active Ingrediant or Action Taken", into=c("D","Active Ingrediant or Action Taken","E"),sep=c(1,-2)) %>%
-  separate("Active Ingrediant or Action Taken", into=c("Active Ingrediant or Action Taken","EPA Pesticide Chemical Code"),sep="=") %>%
+  separate(`Active Ingrediant or Action Taken`, into=c("D","Active Ingrediant or Action Taken","E"),sep=c(1,-2)) %>%
+  separate(`Active Ingrediant or Action Taken`, into=c("Active Ingrediant or Action Taken","EPA Pesticide Chemical Code"),sep="=") %>%
   select(-A,-Label,-C,-D,-E)
 
 # get restricted use chemicals
@@ -28,15 +28,25 @@ veg.chem <- veg.tidy %>%
   filter(Domain=="RESTRICTED USE CHEMICAL") %>%
   select(Domain:`EPA Pesticide Chemical Code`) %>% 
   unique() %>%
-  arrange(Type)
+  arrange(Type) %>%
+  dplyr::rename(`Active Ingrediant`=`Active Ingrediant or Action Taken`)
 
 # table for toxicity
 toxicity <- tibble(
-  `Toxicity Measurements` = c("20-150 mg/kg", "5620-8350 mg/kg", "10 mg/kg",
+  `Toxicity Measurements (oral, for an experimental rat)` = c("20-150 mg/kg", "5620-8350 mg/kg", "10 mg/kg",
                               "380 - 651 mg/kg", "54-70 mg/kg", "5000 mg/kg",
-                              "2000 mg/kg", "869 - 1271 mg/kg")
+                              "2000 mg/kg", "869 - 1271 mg/kg", "3129 mg/kg",
+                              "458 mg/kg", "450 mg/kg", "144 mg/kg",
+                              "12-48 mg/kg", "50-281 mg/kg", "50 mg/kg",
+                              "430-4000 mg/kg", "1563 mg/kg", "86 mg/kg",
+                              "2.75-720 mg/kg", "60-387 mg/kg", "1.9-12.5 mg/kg",
+                              "66.7-70.6 mg/kg", "150-2000 mg/kg", ">4640 mg/kg",
+                              "55 mg/kg", "16-21 mg/kg", "39.1-398 mg/kg", "115-165 mg/kg"
+                              )
 )
 
-# information taken from www.fao.org, https://pubchem.ncbi.nlm.nih.gov, and pmep.cce.cornell.edu
+# information taken from www.fao.org, https://pubchem.ncbi.nlm.nih.gov, pmep.cce.cornell.edu, 
+# https://www.bartlett.com, and https://sitem.herts.ac.uk/
 
 # join veg.chem and toxicity tables together
+veg.chem <- veg.chem %>% bind_cols(toxicity)
